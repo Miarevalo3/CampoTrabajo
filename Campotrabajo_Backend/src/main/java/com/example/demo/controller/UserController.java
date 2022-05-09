dcb57b0cdb660d82b43c3ca776947194df83d973
LuisHT02 <79728810+LuisHT02@users.noreply.github.com>
2022-05-09 17:12:49 -0500

    .
    .

--- Campotrabajo_Backend/src/main/java/com/example/demo/controller/UserController.java
+++ Campotrabajo_Backend/src/main/java/com/example/demo/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
+
 @RestController // Defines that this class is a spring bean
 @RequestMapping("/api/v1/")
 public class UserController {
